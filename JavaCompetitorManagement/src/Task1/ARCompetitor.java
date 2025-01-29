7af5f198c733947da3f12d2f01347bf58f6d06c3
anwrat <anweshrwt22@gmail.com>
2025-01-29 18:45:15 +0545

    Added Competitor and Name class file

---
+++ JavaCompetitorManagement/src/Task1/ARCompetitor.java
@@ -0,0 +1,14 @@
+package Task1;
+
+/**
+ * Competitor class to hold the details for every competitor
+ */
+public class ARCompetitor {
+	private int compID;
+	private int age;
+	private String country;
+	private String level;
+	public ARCompetitor() {
+		
+	}
+}
