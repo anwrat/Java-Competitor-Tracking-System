870f2b574d114ad9571139996320dd4c42f3c38a
anwrat <anweshrwt22@gmail.com>
2025-01-29 20:48:30 +0545

    Adding getter and setter methods

--- JavaCompetitorManagement/src/Task1/ARCompetitor.java
+++ JavaCompetitorManagement/src/Task1/ARCompetitor.java
@@ -11,4 +11,60 @@ public class ARCompetitor {
 	public ARCompetitor() {
 		
 	}
+	/**
+	 * setter method for Competitor ID
+	 * @param id
+	 */
+	public void setcompID(int id) {
+		compID=id;
+	}
+	/**
+	 * setter method for Competitor Age
+	 * @param age
+	 */
+	public void setage(int age) {
+		this.age=age;
+	}
+	/**
+	 * setter method for Competitor country
+	 * @param country
+	 */
+	public void setcountry(String country) {
+		this.country=country;
+	}
+	/**
+	 * setter method for Competitor level
+	 * @param level
+	 */
+	public void setlevel(String level) {
+		this.level=level;
+	}
+	/**
+	 * getter method for Competitor ID
+	 * @return Competitor ID
+	 */
+	public int getcompID() {
+		return compID;
+	}
+	/**
+	 * getter method for Competitor age
+	 * @return Competitor age
+	 */
+	public int getage() {
+		return age;
+	}
+	/**
+	 * getter method for Competitor country
+	 * @return Competitor country
+	 */
+	public String getcountry() {
+		return country;
+	}
+	/**
+	 * getter method for Competitor level
+	 * @return Competitor level
+	 */
+	public String getlevel() {
+		return level;
+	}
 }
