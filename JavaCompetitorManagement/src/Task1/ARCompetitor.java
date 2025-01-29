9937d171d9b3a6f34b4f0267a473cffaa5822d7f
anwrat <anweshrwt22@gmail.com>
2025-01-29 21:51:22 +0545

    Adding methods for name class and removing javadocs for now

--- JavaCompetitorManagement/src/Task1/ARCompetitor.java
+++ JavaCompetitorManagement/src/Task1/ARCompetitor.java
@@ -1,70 +1,53 @@
 package Task1;
 
-/**
- * Competitor class to hold the details for every competitor
- */
 public class ARCompetitor {
 	private int compID;
+	private Name name;
 	private int age;
 	private String country;
 	private String level;
-	public ARCompetitor() {
-		
+	public ARCompetitor(int id,Name name,int age,String country,String level) {
+		compID=id;
+		this.name=name;
+		this.age=age;
+		this.country=country;
+		this.level=level;
 	}
-	/**
-	 * setter method for Competitor ID
-	 * @param id
-	 */
 	public void setcompID(int id) {
 		compID=id;
 	}
-	/**
-	 * setter method for Competitor Age
-	 * @param age
-	 */
 	public void setage(int age) {
 		this.age=age;
 	}
-	/**
-	 * setter method for Competitor country
-	 * @param country
-	 */
 	public void setcountry(String country) {
 		this.country=country;
 	}
-	/**
-	 * setter method for Competitor level
-	 * @param level
-	 */
 	public void setlevel(String level) {
 		this.level=level;
 	}
-	/**
-	 * getter method for Competitor ID
-	 * @return Competitor ID
-	 */
 	public int getcompID() {
 		return compID;
 	}
-	/**
-	 * getter method for Competitor age
-	 * @return Competitor age
-	 */
 	public int getage() {
 		return age;
 	}
-	/**
-	 * getter method for Competitor country
-	 * @return Competitor country
-	 */
 	public String getcountry() {
 		return country;
 	}
-	/**
-	 * getter method for Competitor level
-	 * @return Competitor level
-	 */
 	public String getlevel() {
 		return level;
 	}
+	public double getOverallScore() {
+		return 5;
+	}
+	public String getFullDetails() {
+		return
+	}
+	public String getShortDetails() {
+		return "CN "+compID+"("+name.getInitials()+") has overall score 5";
+	}
+    @Override
+    public String toString() {
+        return getFullDetails();
+    }
 }
--- JavaCompetitorManagement/src/Task1/Name.java
+++ JavaCompetitorManagement/src/Task1/Name.java
@@ -1,5 +1,50 @@
 package Task1;
 
 public class Name {
+	private String firstname;
+	private String middlename;
+	private String lastname;
+	public Name(String firstname,String middlename,String lastname) {
+		this.firstname=firstname;
+		//Check if middlename is null or empty after removing leading and trailing whitespaces
+		this.middlename = middlename != null && !middlename.trim().isEmpty() ? middlename : null;
+		this.lastname=lastname;
+	}
+	public String getFirstName() {
+        return firstname;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstname = firstName;
+    }
+    public String getMiddleName() {
+    	return middlename;
+    }
+    
+    public void setMiddleName(String middleName) {
+    	this.middlename = middleName;
+    }
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastname = lastName;
+    }
+    public String getInitials() {
+        String initials = Character.toString(firstname.charAt(0)) + Character.toString(lastname.charAt(0));
+        if (middlename != null && !middlename.isEmpty()) {
+            initials =Character.toString(firstname.charAt(0)) + Character.toString(middlename.charAt(0))+Character.toString(lastname.charAt(0));
+        }
+        return initials;
+    }
+
+    @Override
+    public String toString() {
+        if (middlename != null && !middlename.isEmpty()) {
+            return firstname + " " + middlename + " " + lastname;
+        } else {
+            return firstname + " " + lastname;
+        }
+    }
 }
