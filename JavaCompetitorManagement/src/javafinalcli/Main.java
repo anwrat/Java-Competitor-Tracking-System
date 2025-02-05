98af1895aeafc27ded9ce54de9eb94cbd02b50bf
anwrat <anweshrwt22@gmail.com>
2025-02-05 12:57:44 +0545

    Admin logout and show players done

--- JavaCompetitorManagement/src/javafinalcli/Main.java
+++ JavaCompetitorManagement/src/javafinalcli/Main.java
@@ -3,31 +3,39 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner obj=new Scanner(System.in);
-		System.out.println("--- Competitor Management System --");
-		System.out.println("1.User Login/Registration");
-		System.out.println("2.Admin Login");
-		String check;
 		while(true) {
-			System.out.println("Enter your choice(1/2): ");
-			check=obj.next();
-			if(check.equals("1") || check.equals("2")) {
+			Scanner obj=new Scanner(System.in);
+			System.out.println("--- Competitor Management System --");
+			System.out.println("1.User Login/Registration");
+			System.out.println("2.Admin Login");
+			System.out.println("3.Exit");
+			String check;
+			while(true) {
+				System.out.println("Enter your choice(1/2/3): ");
+				check=obj.next();
+				if(check.equals("1") || check.equals("2")|| check.equals("3")) {
+					break;
+				}
+				else {
+					System.out.println("Invalid choice");
+				}
+			}
+			//For Admin 
+			if(check.equals("2")) {
+				AdminDashboard a=new AdminDashboard();
+				a.adminlogin();
+				a.admindashboard();
+			}
+			//For User
+			else if(check.equals("1")) {
+				UserDashboard u=new UserDashboard();
+				u.userlogin();
+			}
+			//For exiting the program
+			else {
+				System.out.println("Thank you for playing");
 				break;
 			}
-			else {
-				System.out.println("Invalid choice");
-			}
-		}
-		//For Admin 
-		if(check.equals("2")) {
-			AdminDashboard a=new AdminDashboard();
-			a.adminlogin();
-			a.admindashboard();
-		}
-		//For User
-		else if(check.equals("1")) {
-			UserDashboard u=new UserDashboard();
-			u.userlogin();
 		}
 	}
 }
